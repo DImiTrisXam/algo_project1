9762a426600d38fac47c4b76eff6565e0e05bc66
DImiTris <49395075+DImiTrisXam@users.noreply.github.com>
2021-10-29 20:45:52 +0300

    hash table

--- utilities/HashTable.cpp
+++ utilities/HashTable.cpp
@@ -1,5 +1,80 @@
-#include "HashTableRe.hpp"
+#include "HashTable.hpp"
+#include <chrono>
+#include <climits>
+#include <cmath>
 #include <iostream>
+#include <random>
+
+/////////////////////////////////////////////
+
+// h_HashFunction functions
+
+float dotProduct(const std::vector<float> &x, const std::vector<float> &y) {
+  float product = 0;
+
+  for (size_t i = 0; i < x.size(); i++)
+    product += x[i] * y[i];
+
+  return product;
+}
+
+int h_HashFunction::hash(std::vector<float> &p) {
+  return (int)floor((dotProduct(p, this->v) + this->t) / (float)this->w);
+}
+
+h_HashFunction::h_HashFunction(int w_, int pSize) : w(w_) {
+  // Create random engine with the help of seed
+  unsigned seed = std::chrono::steady_clock::now().time_since_epoch().count();
+  std::default_random_engine generator(seed);
+
+  std::normal_distribution<float> distN(0.0, 1.0);                  // standard normal distribution
+  std::uniform_real_distribution<float> distU(0.0, (float)this->w); // uniform real distribution
+
+  // pick v's coordinates from normal distribution
+  for (size_t i = 1; i <= pSize; i++)
+    this->v.push_back(distN(generator));
+
+  this->t = distU(generator); // pick t from uniform distribution
+}
+
+h_HashFunction::~h_HashFunction() {
+  std::cout << "h_HashFunction destroyed\n";
+}
+
+/////////////////////////////////////////////
+
+// g_HashFunction functions
+
+int g_HashFunction::ID(std::vector<float> &p, HashTable &table) {
+  int sum = 0;
+
+  for (size_t i = 0; i < table.hfuncs.size(); i++)
+    sum += this->r[i] * table.hfuncs[i]->hash(p);
+
+  return abs(sum % M);
+}
+
+int g_HashFunction::hash(std::vector<float> &p, HashTable &table) {
+  return ID(p, table) % table.size;
+}
+
+g_HashFunction::g_HashFunction(int pSize) {
+  // Create random engine with the help of seed
+  unsigned seed = std::chrono::steady_clock::now().time_since_epoch().count();
+  std::default_random_engine generator(seed);
+
+  std::uniform_int_distribution<int> distU(INT_MIN, INT_MAX); // uniform int distribution
+
+  // pick v's coordinates from normal distribution
+  for (size_t i = 1; i <= pSize; i++)
+    this->r.push_back(distU(generator));
+
+  this->M = UINT_MAX - 4; // 2^32 - 5
+}
+
+g_HashFunction::~g_HashFunction() {
+  std::cout << "g_HashFunction destroyed\n";
+}
 
 /////////////////////////////////////////////
 
@@ -13,49 +88,18 @@ Data::~Data() {
 
 /////////////////////////////////////////////
 
-// hash function
-
-unsigned long djb2(const char *str, size_t tsize) {
-  unsigned long hash = 5381;
-  int c;
-  while (c = *str++)
-    hash = ((hash << 5) + hash) + c; // hash * 33 + c
-  return hash % tsize;
-}
-
-/////////////////////////////////////////////
-
-// Get index of key in table from hashing
-
-size_t getIndex(const char *key, size_t tsize) {
-  unsigned long hash = djb2(key, tsize);
-  return hash % tsize;
-}
-
-/////////////////////////////////////////////
+// HashTable functions
 
 int HashTable::getsize() {
   return size;
 }
 
 void HashTable::insert(Data &input) {
-  size_t index = getIndex(input.key.c_str(), this->size);
+  size_t index = this->gfunc->hash(input.value, *this);
 
   auto buck = std::make_shared<Data>(input);
 
   table[index].push_back(buck); // insert at the last bucket of the chain
-}
-
-std::vector<float> *HashTable::search(std::string key) {
-  size_t index = getIndex(key.c_str(), this->size);
-  Bucket bc = table[index];
-
-  for (const auto &temp : bc) {
-    if (temp->key.compare(key) == 0) // if you find key
-      return &temp->value;
-  }
-
-  return nullptr;
 }
 
 void HashTable::display() {
@@ -74,16 +118,25 @@ void HashTable::display() {
       }
     }
   }
+  std::cout << "\n";
 }
 
-HashTable::HashTable(size_t s /* = 10000*/) : size(s) {
+HashTable::HashTable(int k, int w, int pSize, size_t s /* = 10000*/) : size(s) {
   table = new Bucket[this->size]; // allocate buckets
+
+  for (size_t i = 0; i < k; i++) { // initialize h_i funcs
+    auto h_i = std::make_shared<h_HashFunction>(w, pSize);
+    hfuncs.push_back(h_i);
+  }
+
+  this->gfunc = new g_HashFunction(pSize); // initialize g func
 
   std::cout << "HashTable initialized\n";
 }
 
 HashTable::~HashTable() {
   delete[] table;
+  delete gfunc;
 
   std::cout << "HashTable destroyed\n";
 }
--- utilities/HashTable.hpp
+++ utilities/HashTable.hpp
@@ -5,6 +5,30 @@
 #include <string>
 #include <vector>
 
+class HashTable;
+
+class h_HashFunction {
+  std::vector<float> v;
+  int t;
+  int w;
+
+public:
+  int hash(std::vector<float> &);
+  h_HashFunction(int, int);
+  ~h_HashFunction();
+};
+
+class g_HashFunction {
+  std::vector<float> r;
+  unsigned int M;
+  int ID(std::vector<float> &, HashTable &);
+
+public:
+  int hash(std::vector<float> &, HashTable &);
+  g_HashFunction(int);
+  ~g_HashFunction();
+};
+
 struct Data {
   std::string key;
   std::vector<float> value;
@@ -14,17 +38,21 @@ struct Data {
 };
 
 typedef std::vector<std::shared_ptr<Data>> Bucket;
+typedef std::vector<std::shared_ptr<h_HashFunction>> HashArray;
 
 class HashTable {
   Bucket *table;
   size_t size;
+  HashArray hfuncs; // array of h_i funcs, size k
+  g_HashFunction *gfunc;
+
+  friend class g_HashFunction;
 
 public:
   int getsize();
   void insert(Data &);
-  std::vector<float> *search(std::string);
   void display();
-  HashTable(size_t s = 10000);
+  HashTable(int, int, int, size_t s = 10000);
   ~HashTable();
 };
 
