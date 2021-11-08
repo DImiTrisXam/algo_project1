6c2996049280c7b5eff2f823b299062b54c73f91
tania <taniaroussou98@outlook.com>
2021-11-08 23:32:55 +0200

    optimizations in utils

--- utilities/utilities.hpp
+++ utilities/utilities.hpp
@@ -10,8 +10,8 @@ bool parseCubeArgs(int, const char **, std::string &, std::string &, std::string
 bool parseClusterArgs(int, const char **, std::string &, std::string &, std::string &, bool &, std::string &);
 int readNumberOfLines(std::string, int &);
 int readInputFile(std::string &, HashTable **, int);
-int readQueryFile(std::string &, std::string &, const std::string &, int, int, HashTable **, int);
-void printOutputFile(std::ofstream &, const std::string &, std::string &, std::vector<Neighbor> &, std::vector<Neighbor> &,
+std::vector<Data *>* readQueryFile(std::string &);
+void printOutputFile(std::ofstream &, const std::string , const std::string &, std::vector<Neighbor> &, std::vector<Neighbor> &,
                      std::vector<std::string> &, std::chrono::nanoseconds, std::chrono::nanoseconds);
 bool parseConfigFile(std::string &, int &, int &, int &, int &, int &, int &);
 
