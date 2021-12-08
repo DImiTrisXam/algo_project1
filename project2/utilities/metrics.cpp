bf0e98b3d9631ecacbff5ec6e1b85700db46941d
DImiTris <49395075+DImiTrisXam@users.noreply.github.com>
2021-12-08 19:17:30 +0200

    discreteFrechetDist

--- project2/utilities/metrics.cpp
+++ project2/utilities/metrics.cpp
@@ -48,45 +48,34 @@ double discreteFrechetDist(const Curve &x, const Curve &y) {
 
   auto distArray = new double[m1 * m2]; // array for dynamic programming
 
-  std::vector<float> x_1{(float)x.tVec[0], x.vec[0]}; // first element of Curve x
-  std::vector<float> y_1{(float)y.tVec[0], y.vec[0]}; // first element of Curve y
-
-  distArray[0] = euclidianDist(x_1, y_1); // compute distArray[0][0]
-
-  for (auto j = 1; j < m2; ++j) {                       // compute first row
-    std::vector<float> x_1{(float)x.tVec[0], x.vec[0]}; // first element of Curve x
-    std::vector<float> y_j{(float)y.tVec[j], y.vec[j]}; // j-th element of Curve y
-
-    // distArray[j] = distArray[0][j]
-    distArray[j] = std::max(distArray[j - 1], euclidianDist(x_1, y_j));
-  }
-
-  for (auto i = 1; i < m1; ++i) {                       // compute first column
-    std::vector<float> x_i{(float)x.tVec[i], x.vec[i]}; // i-th element of Curve x
-    std::vector<float> y_1{(float)y.tVec[0], y.vec[0]}; // first element of Curve y
-
-    // distArray[i* m2] = distArray[i][0]
-    distArray[i * m2] = std::max(distArray[(i - 1) * m2], euclidianDist(x_i, y_1));
-  }
-
-  for (auto i = 1; i < m1; ++i) { // compute the rest of matrix
-    for (auto j = 1; j < m2; ++j) {
+  for (auto i = 0; i < m1; ++i) { // compute distance matrix
+    for (auto j = 0; j < m2; ++j) {
       // std::cout << "i: " << i << ",j: " << j << '\n';
 
       std::vector<float> x_i{(float)x.tVec[i], x.vec[i]}; // i-th element of Curve x
       std::vector<float> y_j{(float)y.tVec[j], y.vec[j]}; // j-th element of Curve y
 
-      // distArray[(i - 1) * m2 + j] = distArray[i-1][j]
-      auto d1 = distArray[(i - 1) * m2 + j];
-      // distArray[(i - 1) * m2 + j - 1] = distArray[i-1][j-1]
-      auto d2 = distArray[(i - 1) * m2 + j - 1];
-      // distArray[i * m2 + j - 1] = distArray[i][j-1]
-      auto d3 = distArray[i * m2 + j - 1];
+      if (i == 0 && j == 0) // if first cell
+        distArray[0] = euclidianDist(x_i, y_j);
+      else if (i == 0 && j > 0) { // if first row
+        // distArray[j] = distArray[0][j]
+        distArray[j] = std::max(distArray[j - 1], euclidianDist(x_i, y_j));
+      } else if (i > 0 && j == 0) { // if first column
+        // distArray[i* m2] = distArray[i][0]
+        distArray[i * m2] = std::max(distArray[(i - 1) * m2], euclidianDist(x_i, y_j));
+      } else if (i > 0 && j > 0) { // anything else
+        // distArray[(i - 1) * m2 + j] = distArray[i-1][j]
+        auto d1 = distArray[(i - 1) * m2 + j];
+        // distArray[(i - 1) * m2 + j - 1] = distArray[i-1][j-1]
+        auto d2 = distArray[(i - 1) * m2 + j - 1];
+        // distArray[i * m2 + j - 1] = distArray[i][j-1]
+        auto d3 = distArray[i * m2 + j - 1];
 
-      double min = std::min(d1, std::min(d2, d3));
+        double min = std::min(d1, std::min(d2, d3));
 
-      // distArray[i * m2 + j] = distArray[i][j]
-      distArray[i * m2 + j] = std::max(min, euclidianDist(x_i, y_j));
+        // distArray[i * m2 + j] = distArray[i][j]
+        distArray[i * m2 + j] = std::max(min, euclidianDist(x_i, y_j));
+      }
     }
   }
 
