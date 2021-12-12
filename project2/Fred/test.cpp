681848ee36e1a275914aa6993c73bc31e6090d53
DImiTris <49395075+DImiTrisXam@users.noreply.github.com>
2021-12-12 20:31:15 +0200

    Fred library

---
+++ project2/Fred/test.cpp
@@ -0,0 +1,11 @@
+#include "frechet.hpp"
+
+int main(int argc, char const *argv[]) {
+  std::cout << "HELLO\n";
+
+  Curve *c1, *c2;
+
+  // Frechet::Continuous::distance(*c1, *c2);
+
+  return 0;
+}
